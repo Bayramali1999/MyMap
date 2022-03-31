007a4ec29ed4693155f050f9a1d0f30f9c446d48
msi_PC <babdiyev361@gmail.com>
2022-03-31 14:11:59 +0500

    first commit

--- app/src/main/java/com/example/mymap/FilterActivity.kt
+++ app/src/main/java/com/example/mymap/FilterActivity.kt
@@ -8,21 +8,19 @@ import com.example.mymap.splash.SplashFragment
 
 @Suppress("DEPRECATION")
 class FilterActivity : AppCompatActivity() {
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_filter)
+
         supportFragmentManager.beginTransaction().replace(R.id.container, SplashFragment()).commit()
     }
 
     override fun onResume() {
         super.onResume()
+
         (Handler()).postDelayed({
             supportFragmentManager.beginTransaction().replace(R.id.container, FilterFragment())
                 .commit()
         }, 3000)
-
-
     }
 }
